e5e8600b5c8fffe1bb1fe3a691b2072695a631ae
IEMbg <ivan_em_makaveev@abv.bg>
2021-07-27 18:24:39 +0300

    Added basic bit operations and Sandbox
    Class for bit operations that supports shifts, rotations and addition.

---
+++ ShaProj.Algorithm/BitOperations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShaProj.Algorithm
+{
+    public static class BitOperations
+    {
+        private const byte DEFAULT_WORD_SIZE = 32;
+
+        // Adds all words (32 bit values) and returns the sum without the overflowing bits
+        public static uint Add(params uint[] words)
+        {
+            uint memory = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                memory += words[i];
+            }
+
+            return memory;
+        }
+
+        // Rotates a word (32 bit value) to the right
+        public static uint Rotate(uint word, int positions)
+        {
+            positions = ValidatePositions(positions);
+            var mask = (uint)Math.Pow(2, DEFAULT_WORD_SIZE) - 1;
+
+            var primaryShift = (word >> positions) & mask;
+            var excessShift = (word << DEFAULT_WORD_SIZE - positions) & mask;
+
+            return primaryShift | excessShift;
+        }
+
+        // Shifts a word (32 bit value) to the right
+        public static uint Shift(uint word, int positions)
+        {
+            positions = ValidatePositions(positions);
+
+            return word >> positions;
+        }
+
+        // Validates given positions to be under 32 bits
+        private static int ValidatePositions(int positions)
+        {
+            return positions % DEFAULT_WORD_SIZE;
+        }
+    }
+}
---
+++ ShaProj.Sandbox/Program.cs
@@ -0,0 +1,40 @@
+﻿// System Libraries
+using System;
+
+// Downloaded Libraries
+using Figgle;
+
+// Project Libraries
+using ShaProj.Algorithm;
+
+namespace ShaProj
+{
+    class Program
+    {
+        // A SandBox for testing methods
+        static uint SandBox()
+        {
+            return BitOperations.Shift(0b00000000111111110000000011111111, 8);
+        }
+
+        // Format the output of the SandBox
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FiggleFonts.Standard.Render("Hello Sandbox!"));
+            Console.WriteLine(new string('=', 120));
+            Console.WriteLine();
+
+            var res = SandBox();
+            Console.WriteLine(GetBinaryValue(res));
+
+            Console.WriteLine();
+            Console.WriteLine(new string('=', 120));
+        }
+
+        // Turns a 32 bit value into a string in binary format
+        static string GetBinaryValue(uint number)
+        {
+            return Convert.ToString(number, toBase: 2).PadLeft(32, '0');
+        }
+    }
+}
